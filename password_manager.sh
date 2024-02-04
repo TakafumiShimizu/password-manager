e7400f20e7a52e40e866cd296c19f546dedcf613
takafumi <tkf.smz@gmail.com>
2024-02-04 09:37:33 +0900

    test

--- password_manager.sh
+++ password_manager.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
 PASSWORD_FILE="passwords.txt"
+ENCRYPTED_FILE="passwords.txt.gpg"
+
+function addPassword {
+    echo "サービス名を入力してください："
+    read service
+    echo "ユーザー名を入力してください："
+    read username
+    echo "パスワードを入力してください："
+    read -s password
+    echo "$service:$username:$password" >> $PASSWORD_FILE
+    # ファイルを暗号化し、元のファイルを削除
+    gpg -c $PASSWORD_FILE
+    rm $PASSWORD_FILE
+    echo "パスワードの追加は成功しました。"
+}
+
+function getPassword {
+    # 暗号化されたファイルを一時的に復号化
+    gpg -d $ENCRYPTED_FILE > $PASSWORD_FILE
+    echo "サービス名を入力してください："
+    read search_service
+    found=$(grep "^$search_service:" $PASSWORD_FILE)
+    if [ -z "$found" ]; then
+        echo "そのサービスは登録されていません。"
+    else
+        echo $found
+    fi
+    # 復号化した一時ファイルを削除
+    rm $PASSWORD_FILE
+}
 
 while true; do
     echo "パスワードマネージャーへようこそ！"
@@ -8,36 +38,9 @@ while true; do
     read action
 
     case $action in
-        "Add Password")
-            echo "サービス名を入力してください："
-            read service
-            echo "ユーザー名を入力してください："
-            read username
-            echo "パスワードを入力してください："
-            read -s password
-            echo "$service:$username:$password" >> $PASSWORD_FILE
-            echo "パスワードの追加は成功しました。"
-            ;;
-        "Get Password")
-            echo "サービス名を入力してください："
-            read search_service
-            grep "^$search_service:" $PASSWORD_FILE | while read -r line ; do
-                IFS=':' read -ra ADDR <<< "$line"
-                echo "サービス名：${ADDR[0]}"
-                echo "ユーザー名：${ADDR[1]}"
-                echo "パスワード：${ADDR[2]}"
-            done
-            if [ $? -ne 0 ]; then
-                echo "そのサービスは登録されていません。"
-            fi
-            ;;
-        "Exit")
-            echo "Thank you!"
-            exit 0
-            ;;
-        *)
-            echo "入力を間違えています。Add Password/Get Password/Exit から入力してください。"
-            ;;
+        "Add Password") addPassword ;;
+        "Get Password") getPassword ;;
+        "Exit") echo "Thank you!"; exit 0 ;;
+        *) echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。" ;;
     esac
 done
-
